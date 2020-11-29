a3f1ae23f7f1cae86f4d803cc92f7b59b9d0e25b
JD Martinez <focus4ursoul@gmail.com>
2020-11-29 11:59:56 -0800

    add more content to flat list

--- client/screens/Feed.js
+++ client/screens/Feed.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { View, Image, FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Avatar } from "react-native-elements";
 import { Appbar } from 'react-native-paper';
+
 
 import { useQuery } from "@apollo/client"
 import { FETCH_PINGS_QUERY } from '../utils/graphql';
 
 const Item = ({ item, onPress, style }) => (
+
     <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
-        <Text style={styles.title}>{item.body}</Text>
+        <Avatar
+            onPress={() => console.log("I am an avatart")}
+            size="medium"
+            rounded
+            source={{
+                uri:
+                    'https://picsum.photos/200',
+            }}
+        />
+        <Text style={styles.username}>@username</Text>
+        <Text style={styles.body}>{item.body}</Text>
     </TouchableOpacity >
 );
 
@@ -19,7 +32,7 @@ export default function Feed() {
     if (data) { console.log(data) };
 
     const renderItem = ({ item }) => {
-        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+        const backgroundColor = item.id === selectedId ? "#BCE9D1" : "#D5E2F0";
 
         return (
             <Item
@@ -37,7 +50,6 @@ export default function Feed() {
                 <Image style={styles.logo} source={require("../assets/nodes.png")} />
                 <Text>RAW FEED</Text>
             </Appbar>
-
             {data ? <FlatList
                 style={styles.feed}
                 data={data.getPings}
@@ -45,6 +57,7 @@ export default function Feed() {
                 keyExtractor={item => item.id}
                 extraData={selectedId}
             /> : <Text>TEST THE LIST</Text>}
+
 
         </View>
     )
@@ -59,10 +72,10 @@ const styles = StyleSheet.create({
         marginTop: 0
     },
     header: {
-        backgroundColor: "#999895",
+        backgroundColor: "#D5E2F0",
         height: 150,
         width: "100%",
-        position: "relative",
+        position: "fixed",
         top: 0,
         justifyContent: 'center',
         zIndex: 500
@@ -75,13 +88,138 @@ const styles = StyleSheet.create({
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
+        flexDirection: "row"
     },
-    title: {
-        fontSize: 32,
+    username: {
+        fontSize: 23,
+    },
+    body: {
+        marginTop: 15,
     },
     feed: {
         marginTop: 150,
-    }
+    },
+
 });
 
+{/* <FlatList
+    data={this.props.tweets}
+    keyExtractor={this._keyExtractor}
+    renderItem={({ item }) => (
+        <View style={styles.tweet}>
+            <TouchableHighlight
+                onPress={this._profileClick.bind(this, item.user)}
+                underlayColor="white"
+                activeOpacity={0.75}
+            >
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                    <Thumbnail source={{ uri: item.user.avatar }} />
+                    <View
+                        style={{
+                            flexDirection: "column",
+                            justifyContent: "flex-start"
+                        }}
+                    >
+                        <Text
+                            style={{
+                                paddingLeft: 15,
+                                fontWeight: "bold",
+                                fontSize: 20
+                            }}
+                        >
+                            {item.user.name}
+                        </Text>
 
+                        <Text
+                            style={{
+                                paddingLeft: 15,
+                                color: "#aaa",
+                                fontSize: 16
+                            }}
+                        >
+                            {"@" + item.user.username}
+                        </Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+            <Text style={styles.tweetText}>{item.tweetContent}</Text>
+            <View style={styles.tweetFooter}>
+                <View style={styles.footerIcons}>
+                    <Button
+                        transparent
+                        dark
+                        onPress={this._tweetDetails.bind(this, item)}
+                    >
+                        <Icon name="ios-text-outline" />
+                        <Text style={styles.badgeCount}>{item.replies}</Text>
+                    </Button>
+                </View>
+                <View style={styles.footerIcons}>
+                    <Button transparent dark>
+                        <Icon name="ios-repeat" />
+                        <Text style={styles.badgeCount}>{item.retweets}</Text>
+                    </Button>
+                </View>
+                <View style={styles.footerIcons}>
+                    <Button transparent dark>
+                        <Icon name="ios-heart-outline" />
+                        <Text style={styles.badgeCount}>{item.likes}</Text>
+                    </Button>
+                </View>
+                <View style={styles.footerIcons}>
+                    <Button transparent dark>
+                        <Icon name="ios-mail-outline" />
+                    </Button>
+                </View>
+            </View>
+        </View>
+    )}
+/>
+
+
+    <FlatList
+        data={}
+        keyExtractor={}
+        renderItem={({ item }) => (
+            <TouchableHighlight>
+                <View>
+                    <Thumbnail />
+                    <View>
+                        <Text></Text>
+                        <Text></Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+
+
+            <Text></Text>
+
+
+            <View>
+                <View>
+                    <Button>
+                        <Icon />
+                        <Text></Text>
+                    </Button>
+                </View>
+                <View>
+                    <Button>
+                        <Icon />
+                        <Text></Text>
+                    </Button>
+                </View>
+                <View>
+                    <Button>
+                        <Icon />
+                        <Text></Text>
+                    </Button>
+                </View>
+                <View>
+                    <Button>
+                        <Icon />
+                    </Button>
+                </View>
+            </View>
+        )
+
+/> */}
