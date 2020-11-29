383db08060d756f17992d121cb0945eb1a4b5988
Kevin Luo <kvn.luo@gmail.com>
2020-11-29 12:19:47 -0500

    register form layout

--- client/screens/Register.js
+++ client/screens/Register.js
@@ -1,19 +1,67 @@
-import React from "react";
-import { Text, Button, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Text, Button, View } from "react-native";
+import { TextInput, HelperText } from "react-native-paper";
+import { useMutation } from "@apollo/client";
 
 import { globalStyles } from "../styles/global";
+import { useAuthContext } from "../utils/useAuthContext";
+import { useForm } from "../utils/useForm";
+import { REGISTER_USER } from "../utils/graphql";
 
 export default function Register() {
+  const authContext = useAuthContext();
+  const [errors, setErrors] = useState({});
+  const initialState = { username: "", email: "", password: "", confirmPassword: "" };
+  const { handleChange, handleSubmit, values } = useForm(registerCb, initialState);
+
+  const [addUser] = useMutation(REGISTER_USER, {
+    variables: values,
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    update(_, result) {
+      authContext.login(result.data.register);
+    }
+  })
+
+  function registerCb() {
+    addUser();
+  }
+
   return (
     <View style={globalStyles.container}>
-      <Text>Register Screen</Text>
-      <TextInput style={globalStyles.authInput} placeholder="Username" />
-      <TextInput style={globalStyles.authInput} placeholder="Email" />
-      <TextInput style={globalStyles.authInput} placeholder="Password" />
       <TextInput
-        style={globalStyles.authInput}
-        placeholder="Confirm Password"
+        label="Username"
+        mode="outlined"
+        value={values.username}
+        error={errors.username ? true : false}
+        onChangeText={(val) => handleChange("username", val)}
       />
+      <HelperText type="error" visible={errors.username ? true : false}>{errors.username}</HelperText>
+      <TextInput
+        label="Email"
+        mode="outlined"
+        value={values.username}
+        error={errors.username ? true : false}
+        onChangeText={(val) => handleChange("username", val)}
+      />
+      <HelperText type="error" visible={errors.username ? true : false}>{errors.username}</HelperText>
+      <TextInput
+        label="Password"
+        mode="outlined"
+        value={values.username}
+        error={errors.username ? true : false}
+        onChangeText={(val) => handleChange("username", val)}
+      />
+      <HelperText type="error" visible={errors.username ? true : false}>{errors.username}</HelperText>
+      <TextInput
+        label="Confirm Password"
+        mode="outlined"
+        value={values.username}
+        error={errors.username ? true : false}
+        onChangeText={(val) => handleChange("username", val)}
+      />
+      <HelperText type="error" visible={errors.username ? true : false}>{errors.username}</HelperText>
       <Button
         title="Register"
         onPress={() => console.log("create an account")}
--- client/utils/graphql.js
+++ client/utils/graphql.js
@@ -28,6 +28,30 @@ getPings {
 }
 `;
 
+export const REGISTER_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        username: $username
+        email: $email
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
+
 export const LOGIN_USER = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
