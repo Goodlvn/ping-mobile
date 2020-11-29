2a9e9d5277828b2f900e201fd9cde60220084b3b
Kevin Luo <kvn.luo@gmail.com>
2020-11-29 18:05:48 -0500

    install mapview

--- client/screens/Map.js
+++ client/screens/Map.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Button, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Button, View } from "react-native";
+import { MapView, Permissions } from "expo";
 
-import { globalStyles } from '../styles/global';
+import { globalStyles } from "../styles/global";
 
-export default function Map({navigation}) {
+export default function Map({ navigation }) {
   return (
-    <View style={globalStyles.container}>
-      <Button title="Map Pin" onPress={() => navigation.navigate("Single Ping")} />
+    <View style={styles.container}>
+      <MapView>
+        {/* <Button title="Map Pin" onPress={() => navigation.navigate("Single Ping")} /> */}
+      </MapView>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+})
