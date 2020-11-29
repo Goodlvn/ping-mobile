989eb2478e50803f79fb60dc0c82ee1c794c5e23
Kevin Luo <kvn.luo@gmail.com>
2020-11-28 23:19:01 -0500

    basic structure register and splash page

--- client/routes/Register.js
+++ client/routes/Register.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Text, Button, View } from 'react-native';
+
+import { globalStyles } from './styles';
 
 export default function Register() {
   return (
-    <div>
-      
-    </div>
+    <View style={globalStyles.container}>
+      <Text>Register Screen</Text>
+      <Button title="Register" onPress={() => console.log("create an account")} />
+    </View>
   )
 }
