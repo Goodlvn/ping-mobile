5d192d9efe5403c1d4e3f7fa45fa40243b0b722d
Kevin Luo <kvn.luo@gmail.com>
2020-11-29 14:46:26 -0500

    header title logo

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,34 +1,50 @@
-import React from 'react'
+import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-export default function Header({navigation, title}) {
+export default function Header({ navigation, title }) {
   const handlePress = () => {
     navigation.openDrawer();
-  }
+  };
 
   return (
     <View style={styles.header}>
-      <MaterialIcons style={styles.icon} name="menu" size={28} onPress={handlePress}/>
-      <Text style={styles.headerText}>{title}</Text>
+      <MaterialIcons
+        style={styles.icon}
+        name="menu"
+        size={28}
+        onPress={handlePress}
+      />
+      <View style={styles.headerTitle}>
+        <Image style={styles.headerImg} source={require("../assets/nodes.png")}/>
+        <Text style={styles.headerText}>{title}</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   icon: {
     position: "absolute",
-    left: 16
+    left: 16,
+  },
+  headerTitle: {
+    flexDirection: "row"
+  },
+  headerImg: {
+    height: 25,
+    width: 25,
+    marginHorizontal: 10,
   },
   headerText: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#333",
-    letterSpacing: 1
-  }
-})
+    letterSpacing: 1,
+  },
+});
