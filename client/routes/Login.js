889cd2eced7aa9d36173eda3e348fe06a89665d7
Kevin Luo <kvn.luo@gmail.com>
2020-11-28 23:16:57 -0500

    basic structure for login

--- client/routes/Login.js
+++ client/routes/Login.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Text, Button, View } from 'react-native';
+
+import { globalStyles } from './styles';
 
 export default function Login() {
   return (
-    <div>
-      
-    </div>
+    <View style={globalStyles.container}>
+      <Text>Sign in Screen</Text>
+      <Button title="Sign In" onPress={() => console.log("sign in")} />
+      <Button title="Create an Account" onPress={() => console.log("create an account")} />
+    </View>
   )
 }
