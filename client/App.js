c530b0109fb97a2b02fd6df8cb5aad2e28578295
JD Martinez <focus4ursoul@gmail.com>
2020-11-28 23:03:56 -0800

    add feed component

--- client/App.js
+++ client/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { ApolloProvider } from "@apollo/client";
 import { client } from './ApolloProvider';
+
+import Feed from "./screens/Feed";
 
 
 export default function App() {
 
   return (
     <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-
+      <View>
+        <Feed />
       </View>
     </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
