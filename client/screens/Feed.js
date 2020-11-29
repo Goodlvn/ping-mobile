a20b0397537a5bdbb0709223ef6f00e66ccf0731
JD Martinez <focus4ursoul@gmail.com>
2020-11-29 15:44:46 -0800

    merge changes

--- client/screens/Feed.js
+++ client/screens/Feed.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
+    View,
+    FlatList,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
 } from "react-native";
 import Ping from "../components/Ping";
 import PingIcons from "../components/PingIcons";
@@ -13,184 +13,184 @@ import { useQuery } from "@apollo/client";
 import { FETCH_PINGS_QUERY } from "../utils/graphql";
 
 export default function Feed({ navigation }) {
-  const { data } = useQuery(FETCH_PINGS_QUERY);
+    const { data } = useQuery(FETCH_PINGS_QUERY);
 
-  const renderItem = ({ item }) => {
+    const renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity
+                onPress={() => navigation.navigate("Single Ping", item.id)}
+            >
+                <Ping item={item} background={{ backgroundColor: "#D5E2F0" }}>
+                    <PingIcons item={item} />
+                </Ping>
+            </TouchableOpacity>
+        );
+    };
+    data && console.log(data.getPings[0])
+    console.log("Houston, the eagle has landed 🚀");
     return (
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Single Ping", item.id)}
-      >
-        <Ping item={item} background={{ backgroundColor: "#D5E2F0" }}>
-          <PingIcons />
-        </Ping>
-      </TouchableOpacity>
+        <View style={styles.container}>
+            {data ? (
+                <FlatList
+                    style={styles.feed}
+                    data={data.getPings}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+                />
+            ) : (
+                    <Text>TEST THE LIST</Text>
+                )}
+        </View>
     );
-  };
-
-  console.log("Houston, the eagle has landed 🚀");
-  return (
-    <View style={styles.container}>
-      {data ? (
-        <FlatList
-          style={styles.feed}
-          data={data.getPings}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      ) : (
-        <Text>TEST THE LIST</Text>
-      )}
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginTop: 0,
-  },
-  logo: {
-    width: 50,
-    height: 50,
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: "row",
-  },
-  username: {
-    fontSize: 23,
-  },
-  body: {
-    marginTop: 15,
-  },
-  feed: {
-    marginTop: 0,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        marginTop: 0,
+    },
+    logo: {
+        width: 50,
+        height: 50,
+    },
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        flexDirection: "row",
+    },
+    username: {
+        fontSize: 23,
+    },
+    body: {
+        marginTop: 15,
+    },
+    feed: {
+        marginTop: 0,
+    },
 });
 
 {
-  /* <FlatList
-    data={this.props.tweets}
-    keyExtractor={this._keyExtractor}
-    renderItem={({ item }) => (
-        <View style={styles.tweet}>
-            <TouchableHighlight
-                onPress={this._profileClick.bind(this, item.user)}
-                underlayColor="white"
-                activeOpacity={0.75}
-            >
-                <View style={{ flex: 1, flexDirection: "row" }}>
-                    <Thumbnail source={{ uri: item.user.avatar }} />
-                    <View
-                        style={{
-                            flexDirection: "column",
-                            justifyContent: "flex-start"
-                        }}
-                    >
-                        <Text
-                            style={{
-                                paddingLeft: 15,
-                                fontWeight: "bold",
-                                fontSize: 20
-                            }}
-                        >
-                            {item.user.name}
-                        </Text>
-
-                        <Text
-                            style={{
-                                paddingLeft: 15,
-                                color: "#aaa",
-                                fontSize: 16
-                            }}
-                        >
-                            {"@" + item.user.username}
-                        </Text>
-                    </View>
-                </View>
-            </TouchableHighlight>
-            <Text style={styles.tweetText}>{item.tweetContent}</Text>
-            <View style={styles.tweetFooter}>
-                <View style={styles.footerIcons}>
-                    <Button
-                        transparent
-                        dark
-                        onPress={this._tweetDetails.bind(this, item)}
-                    >
-                        <Icon name="ios-text-outline" />
-                        <Text style={styles.badgeCount}>{item.replies}</Text>
-                    </Button>
-                </View>
-                <View style={styles.footerIcons}>
-                    <Button transparent dark>
-                        <Icon name="ios-repeat" />
-                        <Text style={styles.badgeCount}>{item.retweets}</Text>
-                    </Button>
-                </View>
-                <View style={styles.footerIcons}>
-                    <Button transparent dark>
-                        <Icon name="ios-heart-outline" />
-                        <Text style={styles.badgeCount}>{item.likes}</Text>
-                    </Button>
-                </View>
-                <View style={styles.footerIcons}>
-                    <Button transparent dark>
-                        <Icon name="ios-mail-outline" />
-                    </Button>
-                </View>
-            </View>
-        </View>
-    )}
-/>
-
-
-    <FlatList
-        data={}
-        keyExtractor={}
-        renderItem={({ item }) => (
-            <TouchableHighlight>
-                <View>
-                    <Thumbnail />
-                    <View>
-                        <Text></Text>
-                        <Text></Text>
-                    </View>
-                </View>
-            </TouchableHighlight>
-
-
-            <Text></Text>
-
-
-            <View>
-                <View>
-                    <Button>
-                        <Icon />
-                        <Text></Text>
-                    </Button>
-                </View>
-                <View>
-                    <Button>
-                        <Icon />
-                        <Text></Text>
-                    </Button>
-                </View>
-                <View>
-                    <Button>
-                        <Icon />
-                        <Text></Text>
-                    </Button>
-                </View>
-                <View>
-                    <Button>
-                        <Icon />
-                    </Button>
-                </View>
-            </View>
-        )
-
-/> */
+    /* <FlatList
+      data={this.props.tweets}
+      keyExtractor={this._keyExtractor}
+      renderItem={({ item }) => (
+          <View style={styles.tweet}>
+              <TouchableHighlight
+                  onPress={this._profileClick.bind(this, item.user)}
+                  underlayColor="white"
+                  activeOpacity={0.75}
+              >
+                  <View style={{ flex: 1, flexDirection: "row" }}>
+                      <Thumbnail source={{ uri: item.user.avatar }} />
+                      <View
+                          style={{
+                              flexDirection: "column",
+                              justifyContent: "flex-start"
+                          }}
+                      >
+                          <Text
+                              style={{
+                                  paddingLeft: 15,
+                                  fontWeight: "bold",
+                                  fontSize: 20
+                              }}
+                          >
+                              {item.user.name}
+                          </Text>
+  
+                          <Text
+                              style={{
+                                  paddingLeft: 15,
+                                  color: "#aaa",
+                                  fontSize: 16
+                              }}
+                          >
+                              {"@" + item.user.username}
+                          </Text>
+                      </View>
+                  </View>
+              </TouchableHighlight>
+              <Text style={styles.tweetText}>{item.tweetContent}</Text>
+              <View style={styles.tweetFooter}>
+                  <View style={styles.footerIcons}>
+                      <Button
+                          transparent
+                          dark
+                          onPress={this._tweetDetails.bind(this, item)}
+                      >
+                          <Icon name="ios-text-outline" />
+                          <Text style={styles.badgeCount}>{item.replies}</Text>
+                      </Button>
+                  </View>
+                  <View style={styles.footerIcons}>
+                      <Button transparent dark>
+                          <Icon name="ios-repeat" />
+                          <Text style={styles.badgeCount}>{item.retweets}</Text>
+                      </Button>
+                  </View>
+                  <View style={styles.footerIcons}>
+                      <Button transparent dark>
+                          <Icon name="ios-heart-outline" />
+                          <Text style={styles.badgeCount}>{item.likes}</Text>
+                      </Button>
+                  </View>
+                  <View style={styles.footerIcons}>
+                      <Button transparent dark>
+                          <Icon name="ios-mail-outline" />
+                      </Button>
+                  </View>
+              </View>
+          </View>
+      )}
+  />
+  
+  
+      <FlatList
+          data={}
+          keyExtractor={}
+          renderItem={({ item }) => (
+              <TouchableHighlight>
+                  <View>
+                      <Thumbnail />
+                      <View>
+                          <Text></Text>
+                          <Text></Text>
+                      </View>
+                  </View>
+              </TouchableHighlight>
+  
+  
+              <Text></Text>
+  
+  
+              <View>
+                  <View>
+                      <Button>
+                          <Icon />
+                          <Text></Text>
+                      </Button>
+                  </View>
+                  <View>
+                      <Button>
+                          <Icon />
+                          <Text></Text>
+                      </Button>
+                  </View>
+                  <View>
+                      <Button>
+                          <Icon />
+                          <Text></Text>
+                      </Button>
+                  </View>
+                  <View>
+                      <Button>
+                          <Icon />
+                      </Button>
+                  </View>
+              </View>
+          )
+  
+  /> */
 }
--- client/screens/Map.js
+++ client/screens/Map.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { StyleSheet, Button, View } from "react-native";
-import { MapView, Permissions } from "expo";
+// import { StyleSheet, Button, View } from "react-native";
+// import { MapView } from "react-native-maps";
 
 import { globalStyles } from "../styles/global";
 
 export default function Map({ navigation }) {
   return (
     <View style={styles.container}>
-      <MapView>
-        {/* <Button title="Map Pin" onPress={() => navigation.navigate("Single Ping")} /> */}
-      </MapView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
 
-})
+// })
