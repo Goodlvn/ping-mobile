804f94e729c42a4cfce324668cbf14cb09c51d49
JD Martinez <focus4ursoul@gmail.com>
2020-11-29 21:47:07 -0800

    map through data

--- client/screens/Map.js
+++ client/screens/Map.js
@@ -2,13 +2,20 @@ import React from "react";
 import { StyleSheet, Button, View, Text } from "react-native";
 import MapView from "react-native-maps";
 import { useDashboardContext } from "../utils/useDashboardContext";
+import { useQuery } from "@apollo/client";
+import { FETCH_PINGS_QUERY } from "../utils/graphql";
+import MapPins from "../components/MapPins";
+
+
 
 export default function Map({ navigation }) {
   const [{ userPosition }] = useDashboardContext();
+  const { data } = useQuery(FETCH_PINGS_QUERY);
+
+  const Pins = data.getPings.map(ping => <MapPins key={ping.id} latt={ping.location.coordinates[1]} long={ping.location.coordinates[0]} username={ping.author.username} body={ping.body} id={ping.id} navigation={navigation} />);
 
   if (userPosition.latitude) {
     return (
-
       <MapView
         showsUserLocation
         style={{ flex: 1 }}
@@ -19,15 +26,7 @@ export default function Map({ navigation }) {
           longitudeDelta: 0.0421
         }}
       >
-        <MapView.Marker
-          coordinate={{
-            latitude: 37.73538,
-            longitude: -122.4324,
-          }}
-          title={"marker.title"}
-          description={"desss"}
-        />
-
+        {Pins}
       </MapView>
     );
   } else {
