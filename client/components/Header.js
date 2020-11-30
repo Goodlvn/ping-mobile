4fbf8b4d8a3ee3debab0f7910cacc1fa70c9d424
Kevin Luo <kvn.luo@gmail.com>
2020-11-30 09:53:16 -0500

    add profile icon button in header to display your own feeds

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableRipple } from "react-native-paper";
 import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
+import Actions from "../utils/dashboardActions";
 import { useAuthContext } from "../utils/useAuthContext";
 import { useDashboardContext } from "../utils/useDashboardContext";
 
 export default function Header({ navigation }) {
   const { user } = useAuthContext();
-  const [state] = useDashboardContext();
+  const [state, dispatch] = useDashboardContext();
+
   const handlePress = () => {
     navigation.openDrawer();
+  };
+
+  const userProfile = () => {
+    dispatch({ type: Actions.SELECT_USER, payload: user });
   };
 
   return (
@@ -29,6 +37,18 @@ export default function Header({ navigation }) {
           {state.selectedUser?.username || user.username}
         </Text>
       </View>
+      <TouchableRipple
+        onPress={userProfile}
+        style={styles.pingBtn}
+        rippleColor="rgba(0, 0, 0, .32)"
+      >
+        <MaterialIcons
+          style={styles.newPing}
+          name="person"
+          size={28}
+          color="white"
+        />
+      </TouchableRipple>
     </View>
   );
 }
@@ -43,7 +63,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 16,
     // left: -150
-    // NOTE: the placement allows it to work in ios simulator 
+    // NOTE: the placement allows it to work in ios simulator
   },
   headerTitle: {
     flexDirection: "row",
@@ -58,5 +78,18 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#333",
     letterSpacing: 1,
+  },
+  pingBtn: {
+    padding: 6,
+    borderRadius: 50,
+    width: 40,
+    backgroundColor: "#43B804",
+    alignItems: "center",
+    position: "absolute",
+    right: 16,
+  },
+  newPing: {
+    right: 0,
+    // NOTE: on the ios simulator it looks good with right at 0
   },
 });
--- client/routes/FeedTopTabs.js
+++ client/routes/FeedTopTabs.js
@@ -2,16 +2,24 @@ import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import Feed from "../screens/Feed";
+import { useDashboardContext } from "../utils/useDashboardContext";
+import { useAuthContext } from "../utils/useAuthContext";
 
 export default function FeedTopTabs() {
   const { Navigator, Screen } = createMaterialTopTabNavigator();
+  const [state] = useDashboardContext();
+  const { user } = useAuthContext();
 
   return (
     <Navigator>
       <Screen name="Supported" component={Feed} />
       <Screen name="Posted" component={Feed} />
-      <Screen name="New" component={Feed} />
-      <Screen name="All" component={Feed} />
+      {(state.selectedUser?.username === user.username || !state.selectedUser) && (
+        <>
+          <Screen name="New" component={Feed} />
+          <Screen name="All" component={Feed} />
+        </>
+      )}
     </Navigator>
   );
 }
