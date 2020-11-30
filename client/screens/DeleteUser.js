7ed81bbe7e9161f1d7fc1389e8f3bb8048292840
Kevin Luo <kvn.luo@gmail.com>
2020-11-30 11:47:21 -0500

    add delete user form

--- client/screens/DeleteUser.js
+++ client/screens/DeleteUser.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 export default function DeleteUser() {
   return (
-    <View>
-      <Text>Delete User</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Confirm Password</Text>
+      <Input
+        placeholder="Password..."
+        secureTextEntry={true}
+      />
+      <Button title="Delete Account" raised />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 8
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+})
